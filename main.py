e87c3da77c4d832bd6f80f2863e1558e37d03cb2
elciddelapaz <elcid.delapaz@gmail.com>
2023-04-06 20:57:58 +0800

    storing todos locally through a .txt file

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-todos = []
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     match user_action.strip():
         case 'add':
-            todo = input("Enter to do: ")
+            todo = input("Enter to do: ") + '\n'
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             todos.append(todo)
+            file = open('todos.txt', 'w') 
+            file.writelines(todos)
+            file.close()
         case 'show':
-            for i, item in enumerate(todos):
-                print(f"{i+1}-{item}")
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+            for i, j in enumerate(todos):
+                j = j.strip('\n')
+                print(f"{i+1}-{j}")
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             todos[number - 1] = input("New todo: ")
-        case 'exit':
-            break
         case 'complete':
             number = int(input("Number of the todo to complete: "))
             todos.pop(number - 1)
+        case 'exit':
+            break
         case _:
             print("Add, show, or exit only!")
