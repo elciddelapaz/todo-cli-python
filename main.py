231b01ee10084e7d9610070f5357e1b9d015bb42
elciddelapaz <elcid.delapaz@gmail.com>
2023-04-13 22:39:53 +0800

    created functions

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
+def get_todos():
+    with open('todos.txt', 'r') as file:
+        todos = file.readlines()
+    return todos
+def write_todos(todo):
+    with open('todos.txt', 'w') as file:
+        todos = file.writelines(todo)
+    return todos
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
-    if 'add' in user_action:
+    if user_action.startswith("add"):
         todo = user_action[4:]
-        # todo = input("Enter to do: ") + '\n'
-        with open('todos.txt', 'r') as file:
-                todos = file.readlines()
-        todos.append(todo) 
-        with open('todos.txt', 'w') as file:
-            todos = file.writelines(todos)
-    elif 'show' in user_action:
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
+        todos.append(todo + '\n') 
+        todos = write_todos(todos)
+    elif user_action.startswith("show"):
+        todos = get_todos()
         for i, j in enumerate(todos):
                 j = j.strip('\n')
                 print(f"{i+1}-{j}")
-    elif 'edit' in user_action:
-        number = int(user_action[5:])
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
-        new_todo = input("New todo: ")
-        todos[number - 1] = new_todo + "\n"
-        with open('todos.txt', 'w') as file:
-            todos = file.writelines(todos)
-    elif 'complete' in user_action:
-        number = int(user_action[9:])
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
-        todo_to_remove = todos[number - 1].strip('\n')
-        todos.pop(number - 1)
-        with open('todos.txt', 'w') as file:
-            todos = file.writelines(todos)
-        print(f"Removed '{todo_to_remove}'")
-    elif 'exit' in user_action:
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            todos = get_todos()
+            new_todo = input("New todo: ")
+            todos[number - 1] = new_todo + "\n"
+            todos = write_todos(todos)
+        except ValueError:
+            print("Your command is not valid.")
+            continue
+             
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+            todos = get_todos()
+            todo_to_remove = todos[number - 1].strip('\n')
+            todos.pop(number - 1)
+            todos = write_todos(todos)
+            print(f"Removed '{todo_to_remove}'")
+        except IndexError:
+            print("Your command is not valid.")
+            continue
+    elif user_action.startswith("exit"): 
         break
     else:
         print("Command is not valid")
